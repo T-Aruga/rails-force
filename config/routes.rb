09ba037f2c359b7692a25011517bd4ec36adc9bf
T-Aruga <baideneo@gmail.com>
2020-09-21 23:51:31 +0900

    Add paginate normalize and validate

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
       get "login" => "sessions#new", as: :login
       resource :session, only: [ :create, :destroy ]
       resource :account, except: [ :new, :create, :destroy ]
-      # resource :password, only: [ :show, :edit, :update ]
-      # resources :customers
+      resource :password, only: [ :show, :edit, :update ]
     end
   end
 
@@ -19,7 +18,10 @@ Rails.application.routes.draw do
       root "top#index"
       get "login" => "sessions#new", as: :login
       resource :session, only: [ :create, :destroy ]
-      resources :staff_members
+      resources :staff_members do
+        resources :staff_events, only: [ :index ]
+      end
+      resources :staff_events, only: [ :index ]
     end
   end
 
